1ab1cf2430dad843b120abb2ee6cea841ddb8e36
Gregory Burns <greg@burnswebservices.com>
2021-01-04 07:55:07 -0800

    Added modes plan,work,report

--- resources/js/components/AppBar.vue
+++ resources/js/components/AppBar.vue
@@ -7,6 +7,47 @@
     >
         <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                    v-bind="attrs"
+                    v-on="on">{{ currentModeIcon }}
+                </v-icon>
+            </template>
+            <v-list dark dense>
+                <v-list-item @click="setMode('planning')">
+                    <v-list-item-icon>
+                        <v-icon>mdi-clipboard-plus</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>Planing</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click="setMode('working')">
+                    <v-list-item-icon>
+                        <v-icon>mdi-clipboard-flow</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>Working</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click="setMode('reporting')">
+                    <v-list-item-icon>
+                        <v-icon>mdi-clipboard-check</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>Reporting</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+        <span v-if="currentMode === 'planning'">
+            <a v-if="view !== 'board'" v-on:click="showBoard()">
+                <v-icon>mdi-bulletin-board</v-icon>
+            </a>
+            <!-- Calendar Planning -->
+        </span>
+        <span v-if="currentMode === 'working'">
+        <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
                 <v-icon v-bind="attrs"
                         v-on="on">mdi-account-multiple
                 </v-icon>
@@ -32,16 +73,21 @@
         <a v-on:click="toggleProjectNotes()">
             <v-icon>mdi-folder</v-icon>
         </a>
-        <a v-on:click="setView('task-new')" v-if="view !== 'task-new'">
+        <a v-if="view !== 'task-new'" v-on:click="setView('task-new')">
             <v-icon style="background: green">mdi-plus-box-outline</v-icon>
         </a>
             <a v-if="view !== 'board'" v-on:click="showBoard()">
                 <v-icon>mdi-bulletin-board</v-icon>
             </a>
             </span>
+        </span>
+        <span v-if="currentMode === 'reporting'">
+            <!-- Calendar Past events -->
+            <!-- Worklogs/Meetigns/Planning by Project-->
+        </span>
         <v-spacer></v-spacer>
         <v-icon @click="refresh">refresh</v-icon>
-        <clock ></clock>
+        <clock></clock>
     </v-system-bar>
 </template>
 
@@ -53,6 +99,12 @@ import draggable from 'vuedraggable'
 export default {
     name: "AppBar",
     components: {Clock, draggable},
+    data() {
+        return {
+            currentMode: 'working',
+            currentModeIcon: 'mdi-clipboard-flow'
+        };
+    },
     computed: {
         ...mapState([
             'projects',
@@ -68,6 +120,19 @@ export default {
         projectCode: String
     },
     methods: {
+        setMode: function (mode) {
+            console.log('Set Mode', mode)
+            this.currentMode = mode
+            if (mode === 'planning') {
+                this.currentModeIcon = 'mdi-clipboard-plus'
+            }
+            if (mode === 'working') {
+                this.currentModeIcon = 'mdi-clipboard-flow'
+            }
+            if (mode === 'reporting') {
+                this.currentModeIcon = 'mdi-clipboard-check'
+            }
+        },
         setView: function (view) {
             console.log('SetView', view)
             this.$store.dispatch('setView', view)
@@ -94,7 +159,7 @@ export default {
             })
         },
         showBoard: function () {
-            this.$store.dispatch('setView','board')
+            this.$store.dispatch('setView', 'board')
         }
     }
 }
