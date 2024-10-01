5f8cb0c20863d410ddaaebbc5f18eafd11e079e0
greg <greg@burnswebservices.com>
2024-10-01 09:28:51 -0700

    Added React SPA

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,19 @@
+import { Route, Routes,Link } from 'react-router-dom';
+import Dashboard from "./pages/Dashboard";
+import Page2 from "./pages/Page2";
+
 const App = () => {
   return (
-    <div className="App">
-      Hello World
-    </div>
+      <>
+          <div>
+              <Link to="/">Dashboard</Link> &nbsp; &nbsp;
+              <Link to="/page2">Page 2</Link>
+          </div>
+          <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+              <Route path="/page2" element={<Page2/>}/>
+          </Routes>
+      </>
   )
 }
 
