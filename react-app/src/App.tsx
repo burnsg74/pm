587cb6dcf6b753a13299d209779a1f1df50d0313
greg <greg@burnswebservices.com>
2024-10-01 10:11:29 -0700

    Refactor adding header, nav, and main elements

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,23 @@
-import { Route, Routes,Link } from 'react-router-dom';
-import Dashboard from "./pages/Dashboard";
-import Page2 from "./pages/Page2";
+import { Link, Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Page2 from "./pages/Page2"
+import { Navbar } from "react-bootstrap"
 
 const App = () => {
-  return (
-      <>
-          <div>
-              <Link to="/">Dashboard</Link> &nbsp; &nbsp;
-              <Link to="/page2">Page 2</Link>
-          </div>
-          <Routes>
-              <Route path="/" element={<Dashboard/>}/>
-              <Route path="/page2" element={<Page2/>}/>
-          </Routes>
-      </>
-  )
+  return (<>
+    <header>
+      <Navbar fixed="top">
+        <Link to="/">Dashboard</Link> &nbsp; &nbsp;
+        <Link to="/page2">Page 2</Link>
+      </Navbar>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </main>
+  </>)
 }
 
 export default App
