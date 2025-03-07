48a8384d49045bc6e0244bb8c519b81f04262dcf
Greg Burns <greg@burnswebservices.com>
2025-03-07 07:24:40 -0800

    Add Bootstrap integration and configure project settings
    Introduce Bootstrap and Bootstrap Icons for styling and functionality. Update tsconfig.json to include typeRoots and enhance type resolution. Adjust page content, dependency configurations, and remove unused nginx block for improved setup.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import 'bootstrap-icons/font/bootstrap-icons.css';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+</script>
+    <slot />
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,2 @@
+import {DB_NAME} from "$env/static/private"
+console.log(DB_NAME);
